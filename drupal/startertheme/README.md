91883bd4785a1cdeaf8e55e7e386649208b0d6bb
Nate Mow <natemow@gmail.com>
2013-08-14 12:53:11 -0400

    Adding README and cutting JS cruft.

---
+++ drupal/startertheme/README.md
@@ -0,0 +1,7 @@
+h1. D7 Starter Theme
+
+* HTML5 with backwards-compatibility Modernizr.js and normalization SCSS.
+* Built with Compass and Foundation 4.
+* Patched to make Foundation 4 grid system work in lte IE8.
+* Raleway font via Google Fonts (see _settings.scss).
+
--- drupal/startertheme/assets/js/startertheme.js
+++ drupal/startertheme/assets/js/startertheme.js
@@ -8,46 +8,15 @@
   Drupal.behaviors.startertheme = {
     attach: function (context, settings) {
 
-      // Attach pretty forms stuff.
-      Drupal.behaviors.startertheme.prettyForms(context, settings);
+      // Attach someFeature stuff.
+      Drupal.behaviors.startertheme.someFeature(context, settings);
 
     },
-    prettyForms: function(context, settings) {
-      $(document).foundation();
+    someFeature: function(context, settings) {
 
-      // Borrowed from: http://webdesignerwall.com/tutorials/cross-browser-html5-placeholder-text
-      if(!Modernizr.input.placeholder) {
+      // TODO: do someFeature stuff.
 
-        $('[placeholder]', context)
-          .focus(function() {
-            var input = $(this);
-            if (input.val() == input.attr('placeholder')) {
-              input.val('');
-              input.removeClass('placeholder');
-            }
-          })
-          .blur(function() {
-            var input = $(this);
-            if (input.val() == '' || input.val() == input.attr('placeholder')) {
-              input.addClass('placeholder');
-              input.val(input.attr('placeholder'));
-            }
-          })
-          .blur();
-
-        $('[placeholder]', context)
-          .parents('form')
-          .submit(function() {
-            $(this).find('[placeholder]').each(function() {
-              var input = $(this);
-              if (input.val() == input.attr('placeholder')) {
-                input.val('');
-              }
-            });
-          });
-
-      }
-    },
+    }
   }
 
 })(jQuery);
